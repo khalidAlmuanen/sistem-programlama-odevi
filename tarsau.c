0bb8a9d90eadbcec4ef8bd432810e6b2a71dfa34
KHALID ALMUANEN <kly48170@gmail.com>
2026-05-17 01:26:24 +0300

    Dosya formatı kontrolü eklendi. Sadece ASCII dosyaların kabul edilmesi sağlandı.

--- tarsau.c
+++ tarsau.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+
+#define IO_BUF_SIZE 65536
+
+static int is_ascii_text(const char *path) {
+    FILE *fp = fopen(path, "rb");
+    if (!fp) return 0;
+
+    unsigned char buf[IO_BUF_SIZE];
+    size_t n;
+    while ((n = fread(buf, 1, sizeof buf, fp)) > 0) {
+        for (size_t i = 0; i < n; i++) {
+            if (buf[i] > 0x7F) {
+                fclose(fp);
+                return 0;
+            }
+        }
+    }
+    fclose(fp);
+    return 1;
+}
 
 void print_usage(const char *prog) {
     fprintf(stderr, "Kullanim:\n");
@@ -15,11 +36,26 @@ int main(int argc, char *argv[]) {
     }
 
     if (strcmp(argv[1], "-b") == 0) {
-        printf("Arsivleme modu secildi.\n");
-        /* Gelecek gunlerde icerik eklenecek */
+        if (argc < 3) {
+            fprintf(stderr, "Hata: Arsivlenecek dosya belirtilmedi.\n");
+            return 1;
+        }
+
+        for (int i = 2; i < argc; i++) {
+            if (strcmp(argv[i], "-o") == 0) {
+                i++;
+                continue;
+            }
+
+            if (!is_ascii_text(argv[i])) {
+                fprintf(stderr, "%s giriş dosyasının formatı uyumsuzdur!\n", argv[i]);
+                return 1;
+            }
+        }
+        printf("Tum dosyalar ASCII formatina uygun. Arsivleme hazir.\n");
+
     } else if (strcmp(argv[1], "-a") == 0) {
         printf("Cikarma modu secildi.\n");
-        /* Gelecek gunlerde icerik eklenecek */
     } else {
         fprintf(stderr, "Hata: Gecersiz secenek '%s'.\n", argv[1]);
         print_usage(argv[0]);
